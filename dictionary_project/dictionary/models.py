0649dfee1777c710f674b7f96c34ca14a5dcdff2
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-06 20:30:42 +0400

    update model labels

--- dictionary_project/dictionary/models.py
+++ dictionary_project/dictionary/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 class Dictionary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lang_from = models.CharField(max_length=100, null=False)
-    lang_to = models.CharField(max_length=100, null=False)
+    lang_from = models.CharField(max_length=100, null=False, verbose_name="Language From")
+    lang_to = models.CharField(max_length=100, null=False, verbose_name="Language From")
     date_created = models.DateTimeField(auto_now_add=True)
-    color = models.CharField(max_length=10, default='fa5333')
+    color = models.CharField(verbose_name="Color", default='#fa5333', max_length=9)
 
     def __str__(self):
         return f'{self.lang_from} - {self.lang_to} dictionary'
+    
 
 class Word(models.Model):
     dictionary = models.ForeignKey(Dictionary, on_delete=models.CASCADE)

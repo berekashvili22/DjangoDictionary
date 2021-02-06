275f7bd2f5a2f08843cba3e8b04e1f5925e4ec18
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-06 20:32:04 +0400

    add urls for view,create,save dict

--- dictionary_project/dictionary/urls.py
+++ dictionary_project/dictionary/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('dictionaries', views.dictionaries, name="dictionaries"),
-    path('dictionary', views.dictionary, name="dictionary"),
+    path('dictionary/<int:pk>', views.dictionary, name="dictionary"),
+    path('dictionary/new/', views.dictionary_create, name="dictionary_form"),
+    path('dictionary/save/', views.dictionary_save, name="dictionary_save"),
 ]

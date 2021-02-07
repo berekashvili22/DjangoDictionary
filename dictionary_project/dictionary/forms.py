18a55468115988d8dd297711e59a1108c8b5ebbc
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-07 17:32:55 +0400

    create form for Word

--- dictionary_project/dictionary/forms.py
+++ dictionary_project/dictionary/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.widgets import TextInput
-from .models import Dictionary
+from .models import Dictionary, Word
 
 class CreateDictForm(forms.Form):
     lang_from = forms.CharField(label="Language From",max_length=100,
@@ -12,4 +12,13 @@ class CreateDictForm(forms.Form):
     color = forms.CharField(label='Color', max_length=7,
         widget=forms.TextInput(attrs={'type': 'color', 'class': "form-control dict-form color"}))
 
-    
+
+class AddWordForm(forms.Form):
+    original_word = forms.CharField(label="",max_length=45,
+        widget=forms.TextInput(attrs={'class': "form-control word-form", 'placeholder': "ENTER WORD"}))
+
+    translated_word = forms.CharField(label="",max_length=45,
+        widget=forms.TextInput(attrs={'class': "form-control word-form", 'placeholder': "TRANSLATION"}))
+
+    definition = forms.CharField(label="", required=False,
+        widget=forms.TextInput(attrs={'class': "form-control word-form", 'placeholder': "DEFINITION"}, ))

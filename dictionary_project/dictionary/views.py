124c1dee467cf4678d309e4035bab55e5da81956
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-07 17:34:34 +0400

    add logic for saving/paginating words

--- dictionary_project/dictionary/views.py
+++ dictionary_project/dictionary/views.py
@@ -1,36 +1,61 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.views.generic import CreateView
 from . models import Dictionary, Word
-from . forms import CreateDictForm
+from . forms import CreateDictForm, AddWordForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def home(request):
     return render(request, 'dictionary/home.html')
 
 @login_required
 def dictionaries(request):
+    # get all request.user dictionaries and order by date created
     dictionaries = Dictionary.objects.all().filter(user=request.user).order_by('-date_created')
     context = {'dictionaries': dictionaries}
     return render(request, 'dictionary/dictionaries.html', context)
 
 @login_required
 def dictionary(request, pk):
+    # get dictionary by passed primary key
     dictionary = Dictionary.objects.get(pk=pk)
+    form = AddWordForm()
     if request.user == dictionary.user:
+        # get all words from dictionary
         words = Word.objects.all().filter(dictionary=dictionary.id)
-        context = {'dictionary': dictionary, 'words': words}
+        # pagiante
+        paginator = Paginator(words, 25)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = {'dictionary': dictionary, 'form': form, 'page_obj': page_obj}
+        if request.method == "POST":
+            form = AddWordForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                Word.objects.create(
+                    original_word = data['original_word'],
+                    translated_word = data['translated_word'],
+                    definition = data['definition'],
+                    dictionary = dictionary,
+                )
+                # return last on last page of pagination
+                return HttpResponseRedirect('/dictionary/%s?page=%s'  % (dictionary.id, paginator.num_pages))
+    # if request user != dictionary.user return back to dictionaries page  
     else:
         return redirect('dictionaries')
     return render(request, 'dictionary/dictionary.html', context)
 
 @login_required
 def dictionary_create(request):
+    # create dictionary from
     form = CreateDictForm
     context = {'form': form}
     return render(request, 'dictionary/dictionary_form.html', context)
 
 @login_required
 def dictionary_save(request):
+    # if request method is POST validate data and create dictionary
     if request.method == "POST":
         form = CreateDictForm(request.POST)
         if form.is_valid():
@@ -42,6 +67,7 @@ def dictionary_save(request):
                 user = request.user,
             )
             return redirect('dictionaries')
+    #  if request method is not POST do nothing
     else:
         pass
     return redirect('dictionaries')

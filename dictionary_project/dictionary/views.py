fc55e046cfb96291b0390c7d45840e4695880c2a
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-11 21:24:51 +0400

    update table data without page refresh

--- dictionary_project/dictionary/views.py
+++ dictionary_project/dictionary/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.views.generic import CreateView
 from . models import Dictionary, Word
-from . forms import CreateDictForm, AddWordForm
+from . forms import CreateDictForm, AddWordForm, SearchWordForm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
 
 def home(request):
     return render(request, 'dictionary/home.html')
@@ -20,6 +22,7 @@ def dictionary(request, pk):
     # get dictionary by passed primary key
     dictionary = Dictionary.objects.get(pk=pk)
     form = AddWordForm()
+    # form1 = SearchWordForm()  --- ???  
     if request.user == dictionary.user:
         # get all words from dictionary
         words = Word.objects.all().filter(dictionary=dictionary.id)
@@ -28,8 +31,9 @@ def dictionary(request, pk):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
-        context = {'dictionary': dictionary, 'form': form, 'page_obj': page_obj}
+        context = {'dictionary': dictionary, 'form': form ,'page_obj': page_obj}
         if request.method == "POST":
+            # update word
             form = AddWordForm(request.POST)
             if form.is_valid():
                 data = form.cleaned_data
@@ -71,3 +75,52 @@ def dictionary_save(request):
     else:
         pass
     return redirect('dictionaries')
+
+
+@login_required
+def dictionary_update(request):
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            # get updated word data
+            data = json.loads(request.body)
+            # get word id
+            print(data)
+            exit
+            wordId = data['wordId']
+            action = data['action']
+            # get updated word
+            word = Word.objects.get(pk=wordId)
+            if word.dictionary.user == request.user:
+                # check action type
+                if action == "save":
+                    # update word with new data
+                    word.original_word = data['original']
+                    word.translated_word = data['translated']
+                    word.definition = data['definition']
+                    word.save()
+                if action == "delete":
+                    word.delete()
+            else:
+                return redirect('home')
+
+            return JsonResponse(
+                {'originalWord': data['original'],
+                'translatedWord': data['translated'],
+                'definition': data['definition'],
+                'wordId': data['wordId'],
+                'action': data['action']},
+                 safe=False)
+    else:
+        return redirect('home')
+
+@login_required
+def dictionary_filter(request):
+    if request.method == 'POST':
+        words = Word.filter(
+            original_word__starts_with=searchOriginal) | Word.filter(
+            translated_word__starts_with=searchTranslated) | Word.filter(
+            definition_word__starts_with=searchDefinition)
+
+        data = words.values()
+
+        return JsonResponse(list(data), safe=False)

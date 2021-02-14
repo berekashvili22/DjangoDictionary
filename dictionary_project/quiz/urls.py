600d085c9ae4008844b68adf24b1b0e89dcc7ef7
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-14 18:38:22 +0400

    create quiz urls

---
+++ dictionary_project/quiz/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('quiz', views.quiz_home, name="quiz-home"),
+    path('quiz/add/', views.quiz_form, name="quiz-form"),
+    path('quiz/live/', views.quiz_create, name="quiz-create"),
+]

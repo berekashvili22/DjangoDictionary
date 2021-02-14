7b48a3c9e5d8bb6f16f2b3909668047f8dc6c67a
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-14 18:39:42 +0400

    model form for quiz

--- dictionary_project/quiz/forms.py
+++ dictionary_project/quiz/forms.py
@@ -7,26 +7,3 @@ class CreateQuizForm(ModelForm):
     class Meta:
         model = Quiz
         fields = ['dictionary', 'words_filter', 'length']
-
-
-
-
-#     dictionary = models.ForeignKey(Dictionary, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-#     RANDOM = 'RN'
-#     NEW_WORDS = 'LA'
-#     OLD_WORDS = 'OW'
-
-#     WORDS_FILTER_CHOICES = (
-#         (RANDOM, 'Random'),
-#         (NEW_WORDS, 'New Words'),
-#         (OLD_WORDS, 'Old Words'),
-#     )
-
-#     words_filter = models.CharField(max_length=255, verbose_name="Words Filter", choices=WORDS_FILTER_CHOICES)
-#     length = models.IntegerField(default=20)
-#     is_complete = models.BooleanField(default=False)
-#     date_created = models.DateTimeField(auto_now_add=True)
-
-#     dictionary = forms.CharField(label='Dictionary?', widget=forms.Select(choices=FRUIT_CHOICES))

969e5ab2b9c343d773c0ed233c478e20dab1390c
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-15 20:44:34 +0400

    quiz demo

--- dictionary_project/quiz/urls.py
+++ dictionary_project/quiz/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quiz', views.quiz_home, name="quiz-home"),
+    path('quiz/', views.quiz_home, name="quiz-home"),
     path('quiz/add/', views.quiz_form, name="quiz-form"),
     path('quiz/live/', views.quiz_create, name="quiz-create"),
+    path('quiz/write/<int:pk>', views.quiz, name="quiz"),
 ]

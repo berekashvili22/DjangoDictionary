a6bbf52a89c12aeb5662812127acf5cd5a732ab7
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-21 20:14:29 +0400

    created result urls

--- dictionary_project/quiz/urls.py
+++ dictionary_project/quiz/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('quiz/', views.quiz_home, name="quiz-home"),
     path('quiz/create/', views.quiz_form, name="quiz-form"),
     path('quiz/generate/', views.quiz_create, name="quiz-create"),
-    path('quiz/live/<int:pk>', views.quiz, name="quiz"),
+    path('quiz/live/<int:pk>', views.quiz, name="quiz-live"),
+    path('quiz/result/', views.result_save, name="result-save"),
+    path('quiz/result/view/<int:pk>', views.result, name="result-view"),
 ]

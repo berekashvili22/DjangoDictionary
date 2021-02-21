149996fc7b96bfa3eeaf6be7dfd878ea1387894f
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-02-21 20:11:11 +0400

    change str

--- dictionary_project/dictionary/models.py
+++ dictionary_project/dictionary/models.py
@@ -4,13 +4,12 @@ from django.contrib.auth.models import User
 class Dictionary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     lang_from = models.CharField(max_length=100, null=False, verbose_name="Language From")
-    lang_to = models.CharField(max_length=100, null=False, verbose_name="Language From")
+    lang_to = models.CharField(max_length=100, null=False, verbose_name="Language To")
     date_created = models.DateTimeField(auto_now_add=True)
     color = models.CharField(verbose_name="Color", default='#fa5333', max_length=9)
 
     def __str__(self):
-        return f'{self.lang_from} - {self.lang_to} dictionary'
-    
+        return f'{self.lang_from} - {self.lang_to} Dictionary'    
 
 class Word(models.Model):
     dictionary = models.ForeignKey(Dictionary, on_delete=models.CASCADE)
@@ -20,5 +19,7 @@ class Word(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.original_word}'
+        return f'{self.id}: {self.original_word}'
+
+
 

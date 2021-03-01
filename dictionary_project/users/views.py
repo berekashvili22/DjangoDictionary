2c2c9f61851b1a69973b456ccdd4f0116cb1e411
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-01 22:53:22 +0400

    create  views for profile update

--- dictionary_project/users/views.py
+++ dictionary_project/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.views.generic import UpdateView
+from . models import Profile
 
 def register(request):
     if request.method == 'POST':
@@ -16,4 +18,25 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 def settings(request):
-    return render(request, 'users/settings.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('settings')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {
+        'u_form': u_form,
+    }
+    return render(request, 'users/settings.html', context)
+
+def update_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        imgName = request.POST['choice']
+        image=f'{imgName}.png'
+        Profile.objects.filter(user=user).update(image=image)
+
+    return redirect('home')

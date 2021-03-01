6b142bc4eb01c56876208d11b2d6599e928366a6
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-01 22:50:15 +0400

    about/contact urls

--- dictionary_project/dictionary/urls.py
+++ dictionary_project/dictionary/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path('dictionaries', views.dictionaries, name="dictionaries"),
     path('dictionary/<int:pk>', views.dictionary, name="dictionary"),
     path('dictionary/new/', views.dictionary_create, name="dictionary_form"),

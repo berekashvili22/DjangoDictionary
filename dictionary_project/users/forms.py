43036dbafd4019a2991f7ea0a2bd5c2d97807b2d
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-01 22:52:31 +0400

    profile update form demo

--- dictionary_project/users/forms.py
+++ dictionary_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from . models import Profile
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
@@ -12,6 +13,29 @@ class UserRegisterForm(UserCreationForm):
         email = self.cleaned_data.get('email')
         # check if user with form.email exists
         user_count = User.objects.filter(email=email).count()
+        # if len(email) <= 0:
+            # raise forms.ValidationError("Email field is required")
         if user_count > 0:
             raise forms.ValidationError("A user with that email address already exists.")
         return email
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        # check if user with form.email exists
+        user_count = User.objects.filter(email=email).count()
+        # if len(email) <= 0:
+            # raise forms.ValidationError("Email field is required")
+        if user_count > 0:
+            raise forms.ValidationError("A user with that email address already exists.")
+        return email
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
